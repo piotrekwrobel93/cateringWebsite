2c73e0a452ec072931a6e5abcd6397e8f42a5193
piwkodev <piotrwrobeldev@gmail.com>
2021-02-19 18:27:49 +0000

    added arrows for non-supported wheel event or notebooks

--- public/icons/DownArrow.jsx
+++ public/icons/DownArrow.jsx
@@ -2,7 +2,7 @@ const DownArrow = ({width,height, fill, onClick}) => {
 
 	return (
 		<svg  xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill={fill || "#fff"} clipRule="evenodd" onClick={onClick}>
-			<path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"/>
+			<path strokeWidth={2} stroke={fill || "#fff"} d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"/>
 		</svg>
 	)
 }
